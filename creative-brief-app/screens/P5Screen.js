a863abbd7737ae5501b65364a1bf1c35010986d8
190021081 <gjc6@st-andrews.ac.uk>
2023-05-05 14:11:25 +0100

    Sketch land and sky showing

--- creative-brief-app/screens/P5Screen.js
+++ creative-brief-app/screens/P5Screen.js
@@ -1,5 +1,5 @@
 /* This code is adapted from https://github.com/atorov/react-hooks-p5js by Veselin. Accessed 04/05/2023.*/
-import React, { useContext } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 
 import sketch from '../sketch/sketch'
 import P5WrapperConstructor from '../components/P5Wrapper'
@@ -8,12 +8,48 @@ import { Context } from '../Context'
 const P5Wrapper1 = P5WrapperConstructor()
 
 export default function P5Screen() {
+    const canvasWidth = 800;
+    const canvasHeight = 800;
+    const gridDims = [100, 100];
+    const skyDepthFactor = 0.5; // how screen is sky
+    const skyDepth = skyDepthFactor * gridDims[1];
+    const pixelWidth = canvasWidth / gridDims[0];
+    const pixelHeight = canvasHeight / gridDims[1];
+
+    const [pixels, setPixels] = useState([])
+
     const {
         simEnvData,
         raining,
         rabbitInside,
         rabbitActivity,
     } = useContext(Context)
+
+    function worldSetup() {
+        // create the heavens and the earth
+        let tempPixels = [] 
+        for (let row = 0; row < gridDims[0]; row++) {
+            let pixelRow = [];
+            for (let col = 0; col < gridDims[1]; col++) {
+                if (row < skyDepth) {
+                    pixelRow.push(0);
+                } else if (row == skyDepth) {
+                    pixelRow.push(4);
+                } else if (row == skyDepth + 1) {
+                    pixelRow.push(2);
+                } else {
+                    pixelRow.push(1);
+                }
+            }
+            tempPixels.push(pixelRow);
+        }
+        console.log(`pixels: ${tempPixels}`)
+        setPixels(tempPixels);
+    }
+
+    useEffect(() => {
+        worldSetup()
+    }, [])
 
     return (
         <div >
@@ -24,6 +60,11 @@ export default function P5Screen() {
                     raining: raining,
                     rabbitInside: rabbitInside,
                     rabbitActivity: rabbitActivity,
+                    pixels: pixels,
+                    pixelWidth: pixelWidth,
+                    pixelHeight: pixelHeight,
+                    canvasWidth: canvasWidth,
+                    canvasHeight: canvasHeight
                 }}
             /> : false
             }
--- creative-brief-app/sketch/sketch.js
+++ creative-brief-app/sketch/sketch.js
@@ -1,7 +1,6 @@
 /* This code is adapted from https://github.com/atorov/react-hooks-p5js by Veselin. Accessed 04/05/2023.*/
 
-import { checkNearDawnDusk, seasonalDawnDusk } from "../helper_functions/dateAndTime"
-import scale from "../helper_functions/scale"
+import { checkNearDawnDusk, getTimeFromMinutes, seasonalDawnDusk } from "../helper_functions/dateAndTime"
 
 export default function (s) {
     s.state = {}
@@ -9,26 +8,63 @@ export default function (s) {
 
     s.setup = () => {
         s.createCanvas(800, 800)
-
-        console.log("sketch has been initialised")
+        // s.createCanvas(s.state.canvasWidth, s.state.canvasHeight)
+        // console.log("sketch has been initialised with state: ", Object.keys(s.state))
     }
 
     s.draw = () => {
-        s.colorMode(s.HSB);
-        // let hue = s.map(s.state.simEnvData.temp.value, 0, 20, 0, 360)
-        // let saturation = s.state.simEnvData.randomChoice.value
-        // let time = s.state.simEnvData.time.value
-        // if (time > 720) {
-        //     time = 1440 - time
-        // }
-        // let brightness = s.map(time, 0, 720, 0, 100)
-        s.background(...skyColour(s.state.simEnvData.time.value, s.state.simEnvData.season.value, s.state.raining))
+        s.noStroke();
+        // s.colorMode(s.HSB);
+        // s.background(...skyColour(s.state.simEnvData.time.value, s.state.simEnvData.season.value, s.state.raining))
+
+        for (let row = 0; row < s.state.pixels.length; row++) {
+            let y = row * s.state.pixelHeight;
+            for (let col = 0; col < s.state.pixels[row].length; col++) {
+                let x = col * s.state.pixelWidth;
+                s.colorMode(s.HSB);
+                let colour = pixelColour(s.state.pixels[row][col]);
+                if (typeof colour == 'string') {
+                    s.fill(colour)
+                } else {
+                    s.fill(...colour)
+                }
+                s.square(x, y, s.state.pixelWidth, s.state.pixelHeight);
+            }
+        }
+    }
+
+    function pixelColour(int) {
+        switch (int) {
+            case 0:
+                // sky 
+                return skyColour(
+                    s.state.simEnvData.time.value,
+                    s.state.simEnvData.season.value,
+                    s.state.raining
+                )
+            case 1:
+                // dirt 
+                return 'sienna'
+            case 2:
+                // warren wall
+                return 'saddlebrown'
+            case 3:
+                // warren room
+                return 'tan'
+            case 4:
+                // grass
+                return 'forestgreen'
+        }
     }
 
     function skyColour(time, season, raining) {
         let { dawn, dusk } = seasonalDawnDusk[season]
         if (raining) {
-            return [215, 20, 35]
+            if (time > 720) {
+                time = 1440 - time
+            }
+            let brightness = s.map(time, 0, 720, 15, 75)
+            return [215, 20, brightness]
         } else if (checkNearDawnDusk(season, time, 0.5)) {
             // sunrise/sunset 
             return [345, 70, 75]
@@ -39,7 +75,6 @@ export default function (s) {
             //daytime 
             return [180, 75, 75]
         }
-        return [hue, sat, bright]
     }
 }
 
