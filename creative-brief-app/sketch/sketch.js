caaa24416df32e67218de9db6c02c24bf5a88753
190021081 <gjc6@st-andrews.ac.uk>
2023-05-05 13:39:21 +0100

    Square is now sky-coloured

--- creative-brief-app/sketch/sketch.js
+++ creative-brief-app/sketch/sketch.js
@@ -1,5 +1,8 @@
 /* This code is adapted from https://github.com/atorov/react-hooks-p5js by Veselin. Accessed 04/05/2023.*/
 
+import { checkNearDawnDusk, seasonalDawnDusk } from "../helper_functions/dateAndTime"
+import scale from "../helper_functions/scale"
+
 export default function (s) {
     s.state = {}
     // s.dispatch = () => { }
@@ -12,14 +15,31 @@ export default function (s) {
 
     s.draw = () => {
         s.colorMode(s.HSB);
-        let hue = s.map(s.state.simEnvData.temp.value, 0, 20, 0, 360)
-        let saturation = s.state.simEnvData.randomChoice.value
-        let time = s.state.simEnvData.time.value
-        if (time > 720) {
-            time = 1440 - time
+        // let hue = s.map(s.state.simEnvData.temp.value, 0, 20, 0, 360)
+        // let saturation = s.state.simEnvData.randomChoice.value
+        // let time = s.state.simEnvData.time.value
+        // if (time > 720) {
+        //     time = 1440 - time
+        // }
+        // let brightness = s.map(time, 0, 720, 0, 100)
+        s.background(...skyColour(s.state.simEnvData.time.value, s.state.simEnvData.season.value, s.state.raining))
+    }
+
+    function skyColour(time, season, raining) {
+        let { dawn, dusk } = seasonalDawnDusk[season]
+        if (raining) {
+            return [215, 20, 35]
+        } else if (checkNearDawnDusk(season, time, 0.5)) {
+            // sunrise/sunset 
+            return [345, 70, 75]
+        } else if (time < dawn || time > dusk) {
+            //nighttime 
+            return [252, 35, 16]
+        } else {
+            //daytime 
+            return [180, 75, 75]
         }
-        let brightness = s.map(time, 0, 720, 0, 100)
-        s.background(hue, saturation, brightness)
+        return [hue, sat, bright]
     }
 }
 
