15393c05ad15f45f07e152b16e18d14f6beaf5b6
190021081 <gjc6@st-andrews.ac.uk>
2023-05-05 14:21:04 +0100

    Added warren to sketch

--- creative-brief-app/screens/P5Screen.js
+++ creative-brief-app/screens/P5Screen.js
@@ -5,7 +5,7 @@ import sketch from '../sketch/sketch'
 import P5WrapperConstructor from '../components/P5Wrapper'
 import { Context } from '../Context'
 
-const P5Wrapper1 = P5WrapperConstructor()
+const P5Wrapper1 = P5WrapperConstructor("RabbitSim")
 
 export default function P5Screen() {
     const canvasWidth = 800;
@@ -25,9 +25,47 @@ export default function P5Screen() {
         rabbitActivity,
     } = useContext(Context)
 
+
+    // rooms in the warren 
+    const rooms = [
+        // row, col, width, height
+        [10, skyDepth + 1, 5, 10],
+        [15, skyDepth + 6, 10, 5],
+        [20, skyDepth + 6, 5, 10],
+        [25, skyDepth + 11, 25, 10], // room with white rabbit
+        [50, skyDepth + 16, 10, 5],
+        [60, skyDepth + 14, 10, 7], // room with food/bedding
+        [25, skyDepth + 21, 5, 5],
+        [15, skyDepth + 26, 15, 7], // room for hiding
+    ];
+
+    /**
+     * N.B. x and y are relative to gridDims, not canvas
+     */
+    function digRoom(tempPixels, x, y, w, h) {
+        for (var row = y - 1; row <= y + h; row++) {
+            for (var col = x - 1; col <= x + w; col++) {
+                if (tempPixels[row][col] == 1 || tempPixels[row][col] == 2) {
+                    // excavate dirt and wall
+                    if (row == y - 1 || row == y + h || col == x - 1 || col == x + w) {
+                        // wall around excavation
+                        tempPixels[row][col] = 2;
+                    } else {
+                        // room of excavation
+                        tempPixels[row][col] = 3;
+                    }
+                } else if (tempPixels[row][col] == 4) {
+                    // floating grass becomes sky
+                    tempPixels[row][col] = 0;
+                }
+            }
+        }
+        return tempPixels
+    }
+
     function worldSetup() {
         // create the heavens and the earth
-        let tempPixels = [] 
+        let tempPixels = []
         for (let row = 0; row < gridDims[0]; row++) {
             let pixelRow = [];
             for (let col = 0; col < gridDims[1]; col++) {
@@ -43,7 +81,12 @@ export default function P5Screen() {
             }
             tempPixels.push(pixelRow);
         }
-        console.log(`pixels: ${tempPixels}`)
+
+        // excavate warren
+        for (var room of rooms) {
+            digRoom(tempPixels, ...room);
+        }
+
         setPixels(tempPixels);
     }
 
