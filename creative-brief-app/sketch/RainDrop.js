3ce6430513fac008b92bbc12028aae1e60224f9f
190021081 <gjc6@st-andrews.ac.uk>
2023-05-08 18:33:11 +0100

    Rain speed scales to canvas

--- creative-brief-app/sketch/RainDrop.js
+++ creative-brief-app/sketch/RainDrop.js
@@ -1,12 +1,15 @@
 /* The following code is adapted from  https://editor.p5js.org/kelsierose94/sketches/MU2Y21aG0, by kelsierose94. Accessed 05/05/2023. */
 
+import { canvasSideLength } from "./canvasSettings";
+
 export default class RainDrop {
   constructor(width, height) {
     this.x = Math.random() * width;
     this.y = Math.random() * -height;
 
     this.update = function () {
-      this.speed = 5 + Math.random() * 5;
+      let speedBase = canvasSideLength / 160; 
+      this.speed = speedBase + Math.random() * speedBase;
       this.gravity = 1.05;
       this.y = this.y + this.speed * this.gravity;
 
