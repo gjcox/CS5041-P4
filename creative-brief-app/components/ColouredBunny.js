93150e36aa87c33e66833d466649fbe04238c5ff
190021081 <gjc6@st-andrews.ac.uk>
2023-05-09 16:50:15 +0100

    Changed ColouredBunny to icon

--- creative-brief-app/components/ColouredBunny.js
+++ creative-brief-app/components/ColouredBunny.js
@@ -1,11 +1,5 @@
-import { Button } from "react-native-paper";
+import { Avatar } from "react-native-paper";
 
-export default ({h, s, l}) => {
-  return (
-    <Button
-      icon="rabbit"
-      disabled={true}
-      labelStyle={{ color: `hsl(${h}, ${s}%, ${l}%)`, fontSize: 35 }}
-    />
-  );
+export default ({ h, s, l }) => {
+  return <Avatar.Icon icon="rabbit" color={`hsl(${h}, ${s}%, ${l}%)`} />;
 };
