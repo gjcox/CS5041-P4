432662b8aa7482bff7a3ccdb9baf2b7573776d0d
190021081 <gjc6@st-andrews.ac.uk>
2023-05-09 12:20:55 +0100

    Changed from onValue to snapshot approach

--- creative-brief-app/App.js
+++ creative-brief-app/App.js
@@ -10,14 +10,13 @@ import { signInWithCustomToken } from "firebase/auth";
 import {
   equalTo,
   limitToLast,
-  onValue,
   orderByChild,
   query,
-  ref,
+  ref
 } from "firebase/database";
 import { httpsCallable } from "firebase/functions";
-
 import { useAuthState } from "react-firebase-hooks/auth";
+import { useList } from "react-firebase-hooks/database";
 
 import { Context } from "./Context";
 import { auth, database, firebaseToken, functions } from "./Firebase";
@@ -27,6 +26,7 @@ import OLEDText from "./components/OLEDText";
 import RabbitSim, { Activity } from "./components/RabbitSim";
 import Table from "./components/Table";
 import { getMinuteTime, getSeason } from "./helper_functions/dateAndTime";
+import { groupIds } from "./helper_functions/groupIds";
 import scale from "./helper_functions/scale";
 import useInterval from "./helper_functions/useInterval";
 import P5Screen from "./screens/P5Screen";
@@ -94,52 +94,65 @@ export default function App() {
     updateSimValue("time", getMinuteTime(new Date()));
   }, 1000 * 60);
 
+  const snapshots = (groupId) =>
+    useList(
+      user
+        ? query(
+            ref(database, "data"),
+            orderByChild("groupId"),
+            equalTo(groupId),
+            limitToLast(10)
+          )
+        : null
+    );
+
+  const [hueSnapshots, hueLoading, hueError] = snapshots(groupIds.LightHue);
+  const [satSnapshots, satLoading, satError] = snapshots(
+    groupIds.LightSaturation
+  );
+  const [brightSnapshots, brightLoading, brightError] = snapshots(
+    groupIds.LightBrightness
+  );
+  const [motion1Snapshots, motion1Loading, motion1Error] = snapshots(
+    groupIds.Motion1
+  );
+  const [humiditySnapshots, humidityLoading, humidityError] = snapshots(
+    groupIds.OutsideHumidity
+  );
+  const [greySnapshots, greyLoading, greyError] = snapshots(
+    groupIds.GreyRabbitContact
+  );
+  const [whiteSnapshots, whiteLoading, whiteError] = snapshots(
+    groupIds.WhiteRabbitContact
+  );
+
   // On light hue change, update the temperature
-  onValue(
-    query(
-      ref(database, "data"),
-      orderByChild("groupId"),
-      equalTo(21),
-      limitToLast(1)
-    ),
-    (snapshot) => {
-      let newHue = Object.values(snapshot?.val())[0].integer ?? 0; // 0-360
+  useEffect(() => {
+    if (hueSnapshots && (!hueLoading || hueError)) {
+      let snapshot = hueSnapshots[0];
+      let newHue = snapshot.val().integer; // 0-360
       let scaleHueToTemp = scale([0, 360], [0, 20]);
       let scaledTemp = Math.round(
         scaleHueToTemp(Math.max(0, Math.min(360, newHue)))
       );
       updateSimValue("temp", scaledTemp);
     }
-  );
+  }, [hueSnapshots]);
 
   // On light saturation change, update the random choice
-  onValue(
-    query(
-      ref(database, "data"),
-      orderByChild("groupId"),
-      equalTo(22),
-      limitToLast(1)
-    ),
-    (snapshot) => {
-      let newRandomChoice = Math.round(
-        Object.values(snapshot?.val())[0].integer ?? 0
-      ); // 0-100
+  useEffect(() => {
+    if (satSnapshots && (!satLoading || satError)) {
+      let snapshot = satSnapshots[0];
+      let newRandomChoice = Math.round(snapshot.val().integer);
       updateSimValue("randomChoice", newRandomChoice);
     }
-  );
+  }, [satSnapshots]);
 
   // On light brightness change, update the time of day
-  onValue(
-    query(
-      ref(database, "data"),
-      orderByChild("groupId"),
-      equalTo(22),
-      limitToLast(1)
-    ),
-    (snapshot) => {
-      let newBrightness = Math.round(
-        Object.values(snapshot?.val())[0].integer ?? 0
-      );
+  useEffect(() => {
+    if (brightSnapshots && (!brightLoading || brightError)) {
+      let snapshot = brightSnapshots[0];
+      let newBrightness = Math.round(snapshot.val().integer);
       let scaleBrightToTime = scale([0, 100], [0, 720]); // 0-100 -> 0-1440
       let scaledTime = Math.round(
         scaleBrightToTime(Math.max(0, Math.min(100, newBrightness)))
@@ -147,73 +160,51 @@ export default function App() {
       if (newBrightness % 2 == 1) scaledTime = 1440 - scaledTime; // odd values are noon to midnight
       updateSimValue("time", scaledTime);
     }
-  );
+  }, [brightSnapshots]);
 
   // On motion detector 1 change, update visitor presence
-  onValue(
-    query(
-      ref(database, "data"),
-      orderByChild("groupId"),
-      equalTo(3),
-      limitToLast(1)
-    ),
-    (snapshot) => {
-      let newVisitor = (Object.values(snapshot?.val())[0].integer ?? 0) == 1;
+  useEffect(() => {
+    if (motion1Snapshots && (!motion1Loading || motion1Error)) {
+      let snapshot = motion1Snapshots[0];
+      let newVisitor = snapshot.val().integer == 1;
       updateSimValue("visitor", newVisitor);
     }
-  );
+  }, [motion1Snapshots]);
 
   // On change to outside humidity, update simulation state
-  onValue(
-    query(
-      ref(database, "data"),
-      orderByChild("groupId"),
-      equalTo(11),
-      limitToLast(1)
-    ),
-    (snapshot) => {
-      let newHumidity = Object.values(snapshot?.val())[0].integer ?? 0;
+  useEffect(() => {
+    if (humiditySnapshots && (!humidityLoading || humidityError)) {
+      let snapshot = humiditySnapshots[0];
+      let newHumidity = snapshot.val().integer;
       updateSimValue("humidity", newHumidity);
     }
-  );
+  }, [humiditySnapshots]);
 
   const [raining, setRaining] = useState(false);
   const [rabbitInside, setRabbitInside] = useState(false);
   const [rabbitActivity, setRabbitActivity] = useState(Activity.play);
 
-  // On grey rabbit contact, move the rabbit outside
-  onValue(
-    query(
-      ref(database, "data"),
-      orderByChild("groupId"),
-      equalTo(8),
-      limitToLast(1)
-    ),
-    (snapshot) => {
-      let withGreyRabbit =
-        (Object.values(snapshot?.val())[0].integer ?? 0) == 1;
+  // On white rabbit contact, move the rabbit outside
+  useEffect(() => {
+    if (greySnapshots && (!greyLoading || greyError)) {
+      let snapshot = greySnapshots[0];
+      let withGreyRabbit = snapshot.val().integer == 1;
       if (withGreyRabbit && rabbitInside) {
         setRabbitInside(false);
       }
     }
-  );
+  }, [greySnapshots]);
 
   // On white rabbit contact, move the rabbit inside
-  onValue(
-    query(
-      ref(database, "data"),
-      orderByChild("groupId"),
-      equalTo(8),
-      limitToLast(1)
-    ),
-    (snapshot) => {
-      let withWhiteRabbit =
-        (Object.values(snapshot?.val())[0].integer ?? 0) == 1;
+  useEffect(() => {
+    if (whiteSnapshots && (!whiteLoading || whiteError)) {
+      let snapshot = whiteSnapshots[0];
+      let withWhiteRabbit = snapshot.val().integer == 1;
       if (withWhiteRabbit && !rabbitInside) {
         setRabbitInside(true);
       }
     }
-  );
+  }, [whiteSnapshots]);
 
   /* Authenticate with token upon load */
   useEffect(() => {
@@ -265,7 +256,7 @@ export default function App() {
           <View style={styles.container}>
             <View style={{ ...styles.container, flexDirection: "row" }}>
               <P5Screen />
-              <View style={{...styles.container, gap:10}}>
+              <View style={{ ...styles.container, gap: 10 }}>
                 <MicrobitHandler />
                 <OLEDText title="Messages" limitTo={10} width="100%" />
               </View>
