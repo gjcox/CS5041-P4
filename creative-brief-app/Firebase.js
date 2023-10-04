18d97eb0c2b67bf80746d6611110b8c388083bb4
Gabriel <gabrielcoxomega@gmail.com>
2023-10-04 20:07:46 +0100

    Changed firebase config to reflect new backend

--- creative-brief-app/Firebase.js
+++ creative-brief-app/Firebase.js
@@ -3,15 +3,19 @@ import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 import { getFunctions } from "firebase/functions";
 
+// Your web app's Firebase configuration
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDBjUEw_DQNMQsZJWfTtLL0PQJoH-xF0kk",
-  authDomain: "sta-cs5041.firebaseapp.com",
-  databaseURL: "https://sta-cs5041-p4.firebaseio.com",
-  projectId: "sta-cs5041",
-  storageBucket: "sta-cs5041.appspot.com",
-  messagingSenderId: "639987847762",
-  appId: "1:639987847762:web:c5a35616a1aa1cf243458b",
+  apiKey: "AIzaSyB95yyCPBcRytswEjweBc-r8BR98KdqvDA",
+  authDomain: "interactive-rabbit-sim.firebaseapp.com",
+  projectId: "interactive-rabbit-sim",
+  storageBucket: "interactive-rabbit-sim.appspot.com",
+  messagingSenderId: "114253338890",
+  appId: "1:114253338890:web:5d2216f635fcda5ede02f3",
+  measurementId: "G-WS249Z51XZ",
+  databaseURL: "https://interactive-rabbit-sim-default-rtdb.europe-west1.firebasedatabase.app/"
 };
+
 export const firebaseToken = "51c494eb-8642-45b7-8c6a-04839e9a62ea";
 const firebaseApp = initializeApp(firebaseConfig);
 export const auth = getAuth(firebaseApp);
