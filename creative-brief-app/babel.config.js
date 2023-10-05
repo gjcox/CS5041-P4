6eec68229e49ec084874bd023cd1f9c1ed469022
Gabriel <gabrielcoxomega@gmail.com>
2023-10-05 18:52:46 +0100

    Added babel react preset

--- creative-brief-app/babel.config.js
+++ creative-brief-app/babel.config.js
@@ -1,6 +1,14 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
+
+  const presets = [
+    ['babel-preset-expo', { targets: { node: 'current' } }],
+    ['@babel/preset-react', { targets: { node: 'current' } }]
+  ];
+  const plugins = ['@babel/plugin-syntax-jsx']
+  
   return {
-    presets: ['babel-preset-expo'],
+    presets,
+    plugins
   };
 };
